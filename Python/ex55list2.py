cb05ca9e2eaf5a24b65a1fc52b01f97791805270
Anderson0312 <andersonmoura812@gmail.com>
2022-03-23 20:36:01 -0300

    Update ex55list2.py

--- Python/ex55list2.py
+++ Python/ex55list2.py
@@ -1,4 +1,4 @@
-/*
+'''
 teste = list()
 teste.append('luis')
 teste.append(22)
@@ -14,11 +14,12 @@ print (pessoas)
 galera = [['bruna', 22],['Anderson', 21],['lucia', 10],['mria', 45],['marcio',15],['luis', 50]]
 for p in galera:
     print (f' {p[0]} tem {p[1]} anos de idade.')
-    
+'''  
     
 todos = list()
 dado = list()
-for i in range(0, 3):
+totmaior = totmenor = 0
+for i in range(0, 4):
     dado.append(str(input('Nome: ')))
     dado.append(int(input('Idade: ')))
     todos.append(dado[:])
@@ -26,3 +27,8 @@ for i in range(0, 3):
 for pessoa in todos:
     if pessoa[1] >= 21:
         print (f' {pessoa[0]} tem {pessoa[1]} anos e é maior de idade.')
+        totmaior += 1
+    else:
+        print (f' {pessoa[0]} tem {pessoa[1]} anos e é menor de idade.')
+        totmenor += 1
+print(f'temos {totmaior} maiores de idade e {totmenor} e menores de idade.')
